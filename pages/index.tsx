2e2b140be61953da372677ea4fd7a4efd082e2c0
Sean Jaeger <seanljaeger@gmail.com>
2023-03-18 12:34:21 +0700

    Added about section

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,6 +42,38 @@ export default function Home() {
               spare time.
             </p>
           </section>
+          <section id="about" aria-label="About Sean Jaeger" className="mt-24">
+            <div className="header">About Me</div>
+            <p className="text-general mt-6">Hi, I'm Sean.</p>
+            <p className="text-general mt-6">
+              I've had a very non-traditional path into Software Engineering,
+              having taught myself JavaScript online whilst taking my A levels,
+              landing my first full time role and opting to take that instead of
+              attending university.
+            </p>
+            <hr className="w-4  my-6" />
+            <p className="text-general">
+              I've always maintained an extremely pragmatic approach to solving
+              business problems with well designed code.
+            </p>
+            <p className="text-general mt-6">
+              Since my first two years of professional experience, I have taken
+              a hiatus from my professional career and taken the time to learn
+              the theory behind my work deeply. This involved self-teaching
+              Computer Science topics, including but not limited to: Data
+              Structures & Algorithms, Systems Design and Front End
+              technologies.
+            </p>
+            <p className="text-general mt-6">
+              Outside of tech, I have an eclectic mix of interests, from
+              traveling the world as a Digital Nomad, to competing in Mixed
+              Martial Arts to Chess.
+            </p>
+            <p className="text-general mt-6">
+              I'm deeply passionate about tackling big problems in all fields
+              and love to jump in head first to new challenges.
+            </p>
+          </section>
         </div>
       </main>
     </>
