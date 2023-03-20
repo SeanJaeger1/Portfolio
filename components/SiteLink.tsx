c2b80aa0d0165a774c0ff92776e5a31216fe3210
Sean Jaeger <seanljaeger@gmail.com>
2023-03-20 17:29:28 +0800

    Added types for SiteLink props

--- components/SiteLink.tsx
+++ components/SiteLink.tsx
@@ -1,7 +1,14 @@
 import Link from "next/link"
 import Image from "next/image"
 
-export default function SiteLink({ src, text, url, alt }) {
+interface SiteLinkProps {
+  src: string
+  text: string
+  url: string
+  alt: string
+}
+
+export default function SiteLink({ src, text, url, alt }: SiteLinkProps) {
   return (
     <Link className="flex-row flex link-text" href={url} target={"_blank"}>
       <Image width={16} height={16} src={src} alt={alt} className="mx-4" />
