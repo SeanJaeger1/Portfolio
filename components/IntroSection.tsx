872cecc98498d7eeaa22f5e4aa05883665ebd196
Sean Jaeger <seanljaeger@gmail.com>
2024-10-28 11:50:46 +0000

    Fixed errors with svg aspect ratios

--- components/IntroSection.tsx
+++ components/IntroSection.tsx
@@ -2,13 +2,43 @@ import Image from 'next/image'
 import { FC } from 'react'
 import SiteLink from './SiteLink'
 
+interface LinkData {
+  src: string
+  text: string
+  url: string
+  alt: string
+}
+
 const IntroSection: FC = () => {
+  const links: LinkData[] = [
+    {
+      src: '/ExternalLink.svg',
+      text: 'Check out my LeetCode',
+      url: 'https://leetcode.com/seanjaeger/',
+      alt: 'External link icon',
+    },
+    {
+      src: '/LinkedIn.svg',
+      text: 'Check out my LinkedIn',
+      url: 'https://www.linkedin.com/in/sean-jaeger/',
+      alt: 'LinkedIn icon',
+    },
+    {
+      src: '/CV.svg',
+      text: 'View my CV',
+      url: '/Sean-Jaeger-CV.pdf',
+      alt: 'CV icon',
+    },
+  ]
+
   return (
     <section id="intro" aria-label="My introduction as a developer">
       <div className="header font-kaisei">Sean Jaeger</div>
+
       <div className="my-4 text-general">
         Hey, I&apos;m Sean. A start-up focused software engineer.
       </div>
+
       <div className="my-4 flex flex-row h-28">
         <div className="relative w-28 h-28">
           <Image
@@ -16,34 +46,23 @@ const IntroSection: FC = () => {
             alt="Sean Jaeger headshot photo"
             fill
             className="rounded-full object-cover"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            sizes="(max-width: 768px) 112px, 112px"
+            priority
           />
         </div>
-        <div className="flex flex-col justify-between py-2 px-3">
-          <SiteLink
-            src="/ExternalLink.svg"
-            text="Check out my LeetCode"
-            url="https://leetcode.com/seanjaeger/"
-            alt="External link icon"
-          />
-          <SiteLink
-            src="/LinkedIn.svg"
-            text="Check out my LinkedIn"
-            url="https://www.linkedin.com/in/sean-jaeger/"
-            alt="LinkedIn icon"
-          />
-          <SiteLink
-            src="/CV.svg"
-            text="View my CV"
-            url="/Sean-Jaeger-CV.pdf"
-            alt="CV icon"
-          />
+
+        <div className="flex flex-col justify-between py-2 px-9">
+          {links.map((link, index) => (
+            <SiteLink key={index} {...link} />
+          ))}
         </div>
       </div>
+
       <p className="text-general">
         I&apos;m a Front End developer with significant experience working in
         early-stage start up companies.
       </p>
+
       <p className="text-general mt-6">
         I&apos;m passionate about product development and computer science,
         currently competing actively as a competitive programmer in my spare
--- components/SiteLink.tsx
+++ components/SiteLink.tsx
@@ -11,9 +11,22 @@ interface SiteLinkProps {
 
 const SiteLink: FC<SiteLinkProps> = ({ src, text, url, alt }) => {
   return (
-    <Link href={url} className="flex-row flex link-text" target="_blank">
-      <Image src={src} alt={alt} width={16} height={16} className="mx-4" />
-      <div>{text}</div>
+    <Link
+      href={url}
+      className="flex items-center gap-2 text-sm text-general hover:opacity-80 transition-opacity"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="relative w-5 h-5">
+        <Image
+          src={src}
+          alt={alt}
+          fill
+          className="object-contain"
+          sizes="20px"
+        />
+      </div>
+      <span>{text}</span>
     </Link>
   )
 }
