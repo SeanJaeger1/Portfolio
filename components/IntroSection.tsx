00531c051898a956f165878d66a786c23e9ecb68
Sean Jaeger <seanljaeger@gmail.com>
2026-02-07 12:16:30 +0700

    Once more

--- components/IntroSection.tsx
+++ components/IntroSection.tsx
@@ -172,13 +172,13 @@ const IntroSection: FC = () => {
         <h1
           ref={nameRef}
           className="gsap-hidden font-display italic text-[clamp(3.5rem,11vw,9rem)]
-            leading-[0.9] tracking-[-0.02em] text-cinema-text mb-6 pl-2 sm:pl-0"
+            leading-[0.9] tracking-[-0.02em] text-cinema-text mb-6"
         >
           <span className="block" style={{ clipPath: 'inset(-30px -30px 0 -30px)', transform: 'translateZ(0)' }}>
-            <span className="name-line block">Sean</span>
+            <span className="name-line block ml-3 md:ml-0">Sean</span>
           </span>
           <span className="block" style={{ clipPath: 'inset(-30px -30px -30px -30px)', transform: 'translateZ(0)' }}>
-            <span className="name-line block">
+            <span className="name-line block ml-3 md:ml-0">
               Jaeger<span className="text-cinema-accent">.</span>
             </span>
           </span>
